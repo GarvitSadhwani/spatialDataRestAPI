5c2cc0466d1398ac19ee810189ceca53beff4ef0
Garvit Sadhwani <garvit.sadh@gmail.com>
2022-08-25 15:04:35 +0530

    basics

--- main.go
+++ main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	// "html/template"
+	"fmt"
+	"net/http"
+
+	chi "github.com/go-chi/chi/v5"
+)
+
+type Template interface {
+	Execute(w http.ResponseWriter, data interface{})
+}
+
+func StaticHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "my restAPI")
+}
+
+func main() {
+	rt := chi.NewRouter()
+	rt.Get("/", StaticHandler)
+	rt.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "page not found")
+	})
+	fmt.Println("starting server on 8080")
+	http.ListenAndServe(":8080", rt)
+}
